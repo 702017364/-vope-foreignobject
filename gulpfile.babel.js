ea84c605e9bf8a52723c28f9b1ea4bcf7c185638
fshuirong <702017364@qq.com>
2019-09-02 11:42:45 +0800

    添加各种babel插件

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -20,9 +20,29 @@ const getCompileECMA = (file, dest) => () => gulp
     plugins: [
       [
         require('@babel/plugin-proposal-class-properties'),
-        {
-          loose: true,
-        },
+        {loose: true},
+      ],
+      [
+        require('@babel/plugin-proposal-private-methods'),
+        {loose: true},
+      ],
+      [
+        require('@babel/plugin-proposal-function-bind')
+      ],
+      [
+        require('@babel/plugin-proposal-logical-assignment-operators')
+      ],
+      [
+        require('@babel/plugin-proposal-nullish-coalescing-operator')
+      ],
+      [
+        require('@babel/plugin-proposal-optional-chaining')
+      ],
+      [
+        require('@babel/plugin-proposal-partial-application')
+      ],
+      [
+        require('@babel/plugin-proposal-throw-expressions')
       ],
     ],
   }))
